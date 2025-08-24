f4531f16a1462b113516daf6bfc1bf14826e4b04
jitesh143shah <jitesh143shah@gmail.com>
2025-08-24 16:53:49 +0545

    code updated

---
+++ day-12/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Usestate from "./hook/Usestate";
+
+function App() {
+  return (
+    <>
+      <Usestate />
+    </>
+  );
+}
+
+export default App;
---
+++ day-12/src/hook/Usestate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Usestate = () => {
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Usestate;
