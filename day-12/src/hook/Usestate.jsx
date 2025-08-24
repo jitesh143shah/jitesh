7e55ed126d8b05f82fa5e9b673ea4f503c5ef50f
jitesh143shah <jitesh143shah@gmail.com>
2025-08-24 17:14:45 +0545

    increment and decrement doce

--- day-12/src/hook/Usestate.jsx
+++ day-12/src/hook/Usestate.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
-const Usestate = () => {
+function Usestate() {
+  const [data, setData] = useState(0);
+
+  function increaseVal() {
+    setData((a) => a + 1);
+    console.log("Increase");
+  }
+
+  function decreaseVal() {
+    setData((a) => a - 1);
+    console.log("Decrease");
+  }
   return (
-    <>
-      <div>
-        <h1>hello</h1>
-      </div>
-    </>
+    <div>
+      <button onClick={increaseVal}>Increase</button>
+      <h1>{data}</h1>
+      <button onClick={decreaseVal}>Decrease</button>
+    </div>
   );
-};
+}
 
 export default Usestate;
