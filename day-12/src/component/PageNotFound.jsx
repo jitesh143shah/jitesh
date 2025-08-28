d82602963e666fedcf7dbf4135ce93ade785550b
jitesh143shah <jitesh143shah@gmail.com>
2025-08-28 15:49:31 +0545

    error page updated

--- day-12/src/component/PageNotFound.jsx
+++ day-12/src/component/PageNotFound.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { TbFaceIdError } from "react-icons/tb";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <>
+      <div>
+        <div
+          className="flex flex-col gap-10  justify-center items-center bg-red-100 w-full h-full
+        text-6xl  p-20"
+        >
+          <TbFaceIdError />
+          <div>Page Not Found</div>
+          <div>404 Error!!!</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PageNotFound;
